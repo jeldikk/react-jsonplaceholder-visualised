4eadd456bb713eaf27b174c88c675b12602119ba
jeldikk <jeldi.kamal2013@gmail.com>
2021-02-28 09:39:38 +0530

    contains all the Routes and initial rendering point

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
+import React, {useEffect, useState} from 'react'
+import {Helmet} from 'react-helmet'
 import logo from './logo.svg';
-import './App.css';
+import {useDispatch, useSelector} from 'react-redux'
+
+
+
+import {Switch, Route} from "react-router-dom"
+// import './App.css';
+// import {fetchU} from './redux/posts/posts.actions'
+import {fetchUsers} from "./redux/users/users.actions"
+
+
+import PostListPage from './pages/postlist/postlist.page'
+import UserListPage from "./pages/userlist/userlist.page"
+import PostPage from "./pages/post/post.page"
+import UserPage from "./pages/user/user.page"
+import Header from "./components/header/header.component"
+
 
 function App() {
+
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    dispatch(fetchUsers())
+  }, [dispatch])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Helmet>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
+        <title>Interface for jsonplaceholder api as blogpost</title>
+      </Helmet>
+      <Header />
+      {/* <React.StrictMode> */}
+      <main>
+        <Switch>
+          <Route exact path="/users" component={UserListPage} />
+          <Route exact path="/users/:userId" component={UserPage} />
+          <Route exact path="/posts" component={PostListPage} />
+          <Route exact path="/posts/:postId" component={PostPage} />
+        </Switch>
+      </main>
+      {/* </React.StrictMode> */}
+      
     </div>
   );
+
 }
 
 export default App;
