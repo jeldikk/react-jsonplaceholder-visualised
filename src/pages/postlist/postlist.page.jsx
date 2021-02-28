63ec5f0dcde63ffc858d83be28a7ede578f917f0
jeldikk <jeldi.kamal2013@gmail.com>
2021-02-28 08:39:27 +0530

    page to load all posts in card form. with populated likes, dislikes values using faker package. provides a pagination panel to navigate through different pages with out being fetching extra request in between.

---
+++ src/pages/postlist/postlist.page.jsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from "react";
+
+import "./postlist.styles.scss";
+
+import { useDispatch, useSelector } from "react-redux";
+import { Container } from "react-bootstrap";
+import { fetchPosts } from "../../redux/posts/posts.actions";
+import Pagination from "../../components/pagination/pagination.component";
+import PostList from "../../components/post-list/post-list.component";
+
+import { ITEMS_PER_PAGE } from "../../constants/index";
+
+import { getPostsByPageNumber, getPostsLength } from "../../redux/posts/posts.selectors";
+
+const PostListPage = () => {
+  const [currPage, setCurrPage] = useState(0);
+
+  const dispatch = useDispatch();
+  const posts = useSelector((state) => getPostsByPageNumber(state, currPage));
+  const postsLength = useSelector(getPostsLength)
+
+  useEffect(() => {
+    if(posts.length === 0){
+        dispatch(fetchPosts());
+    }
+  }, [dispatch]);
+
+  const onPageChange = (pageNumber) => {
+    const { selected } = pageNumber;
+    setCurrPage(selected);
+  };
+
+  // console.log({posts})
+
+  return (
+    <div className="postlist-page">
+      <Container className="content-wrapper" fluid="md">
+          <h1 className="text-center">Posts</h1>
+        <PostList posts={posts} currPage={currPage} itemsPerPage={ITEMS_PER_PAGE} />
+        <Pagination
+          onPageChange={onPageChange}
+          pageCount={postsLength / ITEMS_PER_PAGE}
+          pageRangeDisplayed={2}
+          marginPagesDisplayed={2}
+        />
+      </Container>
+    </div>
+  );
+};
+
+export default PostListPage;
