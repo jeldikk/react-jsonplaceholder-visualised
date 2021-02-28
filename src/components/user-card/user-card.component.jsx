d74c265cdc30e607bb472fd104b58f12433386ed
jeldikk <jeldi.kamal2013@gmail.com>
2021-02-28 09:38:04 +0530

    Added user-card.component to represent meta information about user in user-list.component on users.page component

---
+++ src/components/user-card/user-card.component.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import "./user-card.styles.scss"
+import {Card, Button} from 'react-bootstrap'
+import {Link} from 'react-router-dom'
+
+const UserCard = ({user}) => {
+    const {name, username, email} = user;
+    return (
+        <Card border="info" className="user-card">
+            <Card.Body>
+                <Card.Title>{name}</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">{username}</Card.Subtitle>
+                <Card.Subtitle className="text-danger">{email}</Card.Subtitle>
+            </Card.Body>
+            <Card.Footer className="text-right">
+                <Link to={`user/${user.id}`}>
+                    <Button variant="primary">Show more ...</Button>
+                </Link>
+            </Card.Footer>
+        </Card>
+    )
+}
+
+export default UserCard
