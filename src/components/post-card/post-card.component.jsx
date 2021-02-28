2d84c301fbe4711202522a6bb974e856330bc5d8
jeldikk <jeldi.kamal2013@gmail.com>
2021-02-28 09:35:32 +0530

    added post-card component to show post as a card in post-list.component on postlist.page

---
+++ src/components/post-card/post-card.component.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import {useSelector} from 'react-redux'
+
+import {Link} from 'react-router-dom'
+import {Button, Card} from 'react-bootstrap'
+
+import {GrLike} from 'react-icons/gr'
+import {GrDislike} from 'react-icons/gr'
+import {FaRegCommentDots} from 'react-icons/fa'
+
+
+import "./post-card.styles.scss"
+
+import {getUserById} from "../../redux/users/users.selectors"
+
+const PostCard = ({post}) => {
+
+    const {userId, id, title, body, created, likes, dislikes, replies} = post;
+    const user = useSelector(state => getUserById(state,userId))
+    // console.log(user)
+    return (
+        <Card className="post-card">
+            <Card.Header>
+                <p className="title">{title.slice(0, 30)}...</p>
+                <Link className="showmore-link" to={`/posts/${id}`}>
+                    <Button variant="success" size="sm">Show more ...</Button>
+                </Link>
+            </Card.Header>
+            <Card.Body>
+                <Card.Text>{body.slice(0,20)}...</Card.Text>
+                <p className="text-muted timestamp">{created.toString()}</p>
+                <div className="likes-dislikes-replies">
+                    <span className="icon"><GrLike /> {likes}</span>
+                    <span className="icon"><GrDislike /> {dislikes}</span>
+                    {/* <span><FaRegCommentDots />{replies}</span> */}
+                </div>
+            </Card.Body>
+            {/* <h4>{title}</h4> */}
+            {/* <p>{body}</p> */}
+            {/* <p>Posted by: <code>{user.name}(@{user.username})</code></p>
+            <p>Created On: <code>{created.toUTCString()}</code></p> */}
+            
+        </Card>
+    )
+}
+
+export default PostCard
