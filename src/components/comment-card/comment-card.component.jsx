c47c2eeb5be1b23eb46ce06f51c6a79986e5dacc
jeldikk <jeldi.kamal2013@gmail.com>
2021-03-01 10:56:44 +0530

    added html to display likes, dislikes and replies count in comment-card

--- src/components/comment-card/comment-card.component.jsx
+++ src/components/comment-card/comment-card.component.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
 import {Card, Accordion} from "react-bootstrap"
-
+import {FcLike, FcDislike, FcComments} from 'react-icons/fc'
 import "./comment-card.styles.scss"
 
 const CommentCard = ({comment}) => {
 
-    const {id, name, body} = comment;
+    const {id, name, body, likes, dislikes, replies} = comment;
 
     return (
         <Card className="comment-card">
@@ -14,7 +14,15 @@ const CommentCard = ({comment}) => {
                 {name}
             </Accordion.Toggle>
             <Accordion.Collapse eventKey={id}>
-                <Card.Body>{body}</Card.Body>
+                <Card.Body>
+                    <p className="body">{body}</p>
+                    <p className="likes-dislikes-replies">
+                        <span className="icon-wrapper"><FcLike /> {likes} </span>
+                        <span className="icon-wrapper"><FcDislike /> {dislikes} </span>
+                        <span className="icon-wrapper"><FcComments /> {replies} </span>
+                    </p>
+                </Card.Body>
+                
             </Accordion.Collapse>
         </Card>
     )
