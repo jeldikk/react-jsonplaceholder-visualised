61584d02f31bfe7380c9bc89c07ada23fa841562
jeldikk <jeldi.kamal2013@gmail.com>
2021-03-01 11:02:30 +0530

    installed redux-devtools-extension and included it using composeWithDevTools function to compose along with other middleware libraries in development mode

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,26 @@
 import {createStore, applyMiddleware} from 'redux'
-
+import {composeWithDevTools} from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
 import logger from 'redux-logger'
 
 import rootReducer from "./root_reducer"
 
+let composedMiddleware = null;
 const middleware = [];
 
-if(process.env.NODE_ENV === 'development'){
-    middleware.push(logger)
-}
 
 middleware.push(thunk)
 
-export default createStore(rootReducer, applyMiddleware(...middleware))
+if(process.env.NODE_ENV === 'development'){
+    middleware.push(logger)
+    composedMiddleware = composeWithDevTools(applyMiddleware(...middleware))
+}
+else{
+    composedMiddleware = applyMiddleware(...middleware)
+}
+
+
+
+
+
+export default createStore(rootReducer, composedMiddleware)
