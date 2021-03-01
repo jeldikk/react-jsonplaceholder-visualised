0651aea84ae5e41ebabddf6b34f7a420857ee076
jeldikk <jeldi.kamal2013@gmail.com>
2021-03-01 15:30:48 +0530

    Added address and company details into markup and also styled details pane. rendered post cards as list at the end

--- src/pages/user/user.page.jsx
+++ src/pages/user/user.page.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import {useParams} from 'react-router-dom'
 import {useSelector} from 'react-redux'
+import LazyLoad from 'react-lazyload'
 
 import "./user.styles.scss"
 import {Container, Row, Col, Card} from 'react-bootstrap'
 
 import {getUserById} from "../../redux/users/users.selectors"
 import {getPostsByUserId} from "../../redux/posts/posts.selectors"
+
+import PostList from "../../components/post-list/post-list.component"
 
 const UserPage = () => {
 
@@ -15,7 +18,7 @@ const UserPage = () => {
     const user = useSelector(state => getUserById(state, parseInt(userId)))
     const posts = useSelector(state => getPostsByUserId(state, parseInt(userId)))
     console.log({user, userId, posts})
-    const {name, username, website, imageUrl, phone, email} = user;
+    const {name, username, website, imageUrl, phone, email, address:{city, geo:{lat,lon}, street, suite, zipcode}, company:{bs, catchPhrase, name: companyName}} = user;
     
     return (
         <div className="user-page">
@@ -27,7 +30,8 @@ const UserPage = () => {
                             <Card.Body>
                                 <Card.Title>{name}</Card.Title>
                                 <Card.Subtitle className="text-muted">{username}</Card.Subtitle>
-                                <code>{email}</code>
+                                <Card.Subtitle className="text-danger">{email}</Card.Subtitle>
+                                <Card.Subtitle className="text-danger">{phone}</Card.Subtitle>
                             </Card.Body>
                         </Card>
                     </Col>
@@ -41,13 +45,35 @@ const UserPage = () => {
                 </Row>
                 <Row>
                     <Col sm={4}>
-                        Here goes the address information, company information of the person
+                        <Card className="address-card shadow shadow-sm">
+                            <Card.Header>{`${name} ( @${username} )`}</Card.Header>
+                            <Card.Body>
+                                <Card.Title className="text-center">Address Details</Card.Title>
+                                <Card.Text>{street}</Card.Text>
+                                <Card.Text>{suite}</Card.Text>
+                                <Card.Text>{city} - {zipcode}</Card.Text>
+                            </Card.Body>
+                            <Card.Body>
+                                <Card.Title className="text-center">
+                                    Company Details
+                                </Card.Title>
+                                <Card.Text>{companyName}</Card.Text>
+                                <Card.Text>{catchPhrase}</Card.Text>
+                                <Card.Text>{bs}</Card.Text>
+                            </Card.Body>
+                        </Card>
                     </Col>
                     <Col sm={8}>
                         Here goes the google map with indicator according to co-ordinates
                     </Col>
                     
                 </Row>
+                <div className="post-list">
+                    <h3 className="text-center">{username} made {posts.length} posts</h3>
+                    <LazyLoad once>
+                        <PostList posts={posts} />
+                    </LazyLoad>
+                </div>
             </Container>
         </div>
     )
