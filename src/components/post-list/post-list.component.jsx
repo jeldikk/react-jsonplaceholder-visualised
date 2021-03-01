bc5f68ac2a388d7f1f82cc7d7a02e65ce3e7fce3
jeldikk <jeldi.kamal2013@gmail.com>
2021-03-01 11:18:54 +0530

    removed all the class component based code

--- src/components/post-list/post-list.component.jsx
+++ src/components/post-list/post-list.component.jsx
@@ -18,46 +18,5 @@ function PostList({posts, currPage, itemsPerPage}) {
     </div>
   );
 }
-/*
-class PostList extends React.Component {
-  componentDidMount() {
-    console.log("component did mount life cycle method");
-    this.props.fetchPosts();
-    //include data fetching API request
-  }
 
-  renderList() {
-    return this.props.posts.map((post) => {
-      return (
-        <div className="item" key={post.id}>
-          <i className="large middle aligned icon user" />
-          <div className="content">
-            <div className="description">
-              <h2>{post.title}</h2>
-              <p>{post.body}</p>
-            </div>
-          </div>
-        </div>
-      );
-    });
-  }
-  render() {
-    console.log(this.props.posts);
-    return <div className="ui relaxed">{this.renderList()}</div>;
-  }
-}
-*/
-// const mapDispatchToProps = (dispatch) => {
-//     return {
-//         fetchPosts: () => dispatch(fetchPosts())
-//     }
-// }
-
-// const mapStateToProps = (state) => {
-//   return {
-//     posts: state.posts.posts,
-//   };
-// };
-
-// export default connect(mapStateToProps, { fetchPosts })(PostList);
 export default PostList
