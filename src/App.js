a1a5948b1c53207cf685bd0c511353c10961b632
jeldikk <jeldi.kamal2013@gmail.com>
2021-03-24 14:32:45 +0530

    Added code-splitting feature by availing suspense and lazy react pattern

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, Suspense} from 'react'
 import {Helmet} from 'react-helmet'
-import logo from './logo.svg';
-import {useDispatch, useSelector} from 'react-redux'
+// import logo from './logo.svg';
+import {useDispatch} from 'react-redux'
 
 
 
@@ -9,13 +9,17 @@ import {Switch, Route} from "react-router-dom"
 // import './App.css';
 // import {fetchU} from './redux/posts/posts.actions'
 import {fetchUsers} from "./redux/users/users.actions"
-
-
-import PostListPage from './pages/postlist/postlist.page'
-import UserListPage from "./pages/userlist/userlist.page"
-import PostPage from "./pages/post/post.page"
-import UserPage from "./pages/user/user.page"
 import Header from "./components/header/header.component"
+
+const Homepage = React.lazy(()=>import("./pages/home/home.page"))
+// import PostListPage from './pages/postlist/postlist.page'
+const PostListPage = React.lazy(()=> import("./pages/postlist/postlist.page"));
+// import UserListPage from "./pages/userlist/userlist.page"
+const UserListPage = React.lazy(() => import("./pages/userlist/userlist.page"));
+// import PostPage from "./pages/post/post.page"
+const PostPage = React.lazy(()=> import("./pages/post/post.page"))
+// import UserPage from "./pages/user/user.page"
+const UserPage = React.lazy(() => import("./pages/user/user.page"))
 
 
 function App() {
@@ -34,13 +38,17 @@ function App() {
       </Helmet>
       <Header />
       {/* <React.StrictMode> */}
-      <main>
-        <Switch>
-          <Route exact path="/users" component={UserListPage} />
-          <Route exact path="/users/:userId" component={UserPage} />
-          <Route exact path="/posts" component={PostListPage} />
-          <Route exact path="/posts/:postId" component={PostPage} />
-        </Switch>
+      <main style={{marginTop: "70px"}}>
+        <Suspense fallback={<div>Loading ...</div>}>
+          <Switch>
+            <Route exact path="/" component={Homepage} />
+            <Route exact path="/users" component={UserListPage} />
+            <Route exact path="/users/:userId" component={UserPage} />
+            <Route exact path="/posts" component={PostListPage} />
+            <Route exact path="/posts/:postId" component={PostPage} />
+          </Switch>
+        </Suspense>
+        
       </main>
       {/* </React.StrictMode> */}
       
